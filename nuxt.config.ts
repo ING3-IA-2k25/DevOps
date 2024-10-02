445f9f65254e7d4874165f87d758cafd9aaf7962
Pablix2601 <pablo.canella@gmail.com>
2024-10-02 14:26:43 +0200

    Ajout des dépendance

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  components: [
+    {
+        path: '~/components',
+        pathPrefix: false,
+    },
+  ],
+  css: ['~/assets/css/main.css'],
+  modules: ['@nuxt/ui', '@nuxt/content', '@nuxt/eslint'],
+  tailwindcss: {
+    cssPath: ['~/assets/css/main.sass', { injectPosition: 'first' }],
+    configPath: '~/tailwind.config.ts',
+  },
 })
