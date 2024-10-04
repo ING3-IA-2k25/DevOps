acc40f48982cc820ee9ebf60dba671a5614ed3ff
Testmen1 <leo.bigot14@mail.com>
2024-10-04 15:44:09 +0200

    modified db.js -> db.ts

--- db/db.ts
+++ db/db.ts
@@ -2,12 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/your-database-name', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb://localhost:3001/test', {});
     console.log('Connected to MongoDB');
-  } catch (error) {
+  } catch (error : any) {
     console.error('Error connecting to MongoDB:', error.message);
   }
 };
